f77df1094aea287273d5c329fa2e9ca4b80e3b95
Vasssya <vasilii.chernobai@gmail.com>
2012-05-16 12:14:51 +0300

    Second try with GitBranch-D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'c6fd34140c6fc52454ede367a063449bac14a431c718723f3e51f3062c693f8d6fbfd5a9a8dceffd4402c04bf3d0119c25f26e86a38a3b20b448e4227862c3dc'
